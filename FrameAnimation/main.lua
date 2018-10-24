f0c9952423615beca25cd5dbcc87032dbdbd956a
leexianze <44256234+leexianze@users.noreply.github.com>
2018-10-24 20:31:47 +0800

    Add files via upload

--- FrameAnimation/main.lua
+++ FrameAnimation/main.lua
@@ -33,7 +33,9 @@ local screenRight
 local params = {
 	{ radius=18, xDir=1, yDir=1, xSpeed=2.5, ySpeed=6.0, r=1, g=0, b=0.1 },
 	{ radius=15, xDir=1, yDir=1, xSpeed=3.5, ySpeed=4.0, r=0.95, g=0.1, b=0.3 },
-	{ radius=22, xDir=1, yDir=-1, xSpeed=5.5, ySpeed=5.0, r=0.9, g=0.2, b=0.5 }
+	{ radius=22, xDir=1, yDir=-1, xSpeed=5.5, ySpeed=5.0, r=0.9, g=0.2, b=0.5 },
+	{ radius=12, xDir=1, yDir=-1, xSpeed=4.5, ySpeed=5.0, r=0.9, g=0.2, b=0.5 },
+	{ radius=30, xDir=1, yDir=-1, xSpeed=6.5, ySpeed=6.0, r=0.3, g=0.1, b=0.4},
 }
 
 local ballCollection = {}
